e9dcc7091148f5459391558b979f8e0b1b80e14d
chrissu777 <christophersu777@gmail.com>
2024-12-09 18:08:37 -0500

    kms builder

--- src/main/java/com/weaponwatch/vidprocessor/aws/KmsService.java
+++ src/main/java/com/weaponwatch/vidprocessor/aws/KmsService.java
@@ -1,2 +1,18 @@
-package com.weaponwatch.vidprocessor.aws;public class KmsService {
+package com.weaponwatch.vidprocessor.aws;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.kms.model.GenerateDataKeyRequest;
+import software.amazon.awssdk.services.kms.model.GenerateDataKeyResponse;
+
+public class KmsService {
+    private final KmsClient kmsClient;
+
+    public KmsService(){
+        this.kmsClient = KmsClient.builder()
+                .credentialsProvider(DefaultCredentialsProvider.create())
+                .region(Region.US_EAST_1)
+                .build();
+    }
 }
