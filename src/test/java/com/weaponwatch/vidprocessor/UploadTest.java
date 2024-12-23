c5a45a1dffd86f8aea4129a3c4ac0e747d7faa9d
chrissu777 <49414356+chrissu777@users.noreply.github.com>
2024-12-22 20:16:11 -0800

    upload to s3 test

--- src/test/java/com/weaponwatch/vidprocessor/UploadTest.java
+++ src/test/java/com/weaponwatch/vidprocessor/UploadTest.java
@@ -2,13 +2,18 @@ package com.weaponwatch.vidprocessor;
 
 import com.weaponwatch.vidprocessor.aws.S3Service;
 
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 public class UploadTest {
-    public static void main(String[] args){
+    public static void main(String[] args) throws URISyntaxException {
         S3Service s3Service = new S3Service();
+        String filePath = "C:\\Users\\chris\\IdeaProjects\\video-processor\\src\\test\\java\\com\\weaponwatch\\vidprocessor\\resources\\test.txt";
         try{
-            s3Service.uploadVideo("weaponwatch-demo", "test-object1", "test.txt");
+            s3Service.uploadVideo("weaponwatch-demo", "test-object1", filePath);
         }catch(Exception e){
-            System.out.println("Bruh");
+            e.printStackTrace();
         }
     }
 }
