538a606b8d59564859a6a24ee25f87e893e96119
chrissu777 <christophersu777@gmail.com>
2024-12-23 18:09:58 -0800

    more encryption slop

--- src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
+++ src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
@@ -9,6 +9,9 @@ import javax.crypto.spec.SecretKeySpec;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.SecureRandom;
 
 public class EncryptionService {
@@ -16,7 +19,9 @@ public class EncryptionService {
     private static final int GCM_TAG_LENGTH = 128;
     private static final int IV_SIZE = 12;
 
-    public void encryptFile(byte[] key) throws Exception{
+    public void encryptFile(byte[] key, String fileInPath, String fileOutPath) throws Exception{
+        Path inAsPath = Paths.get(fileInPath);
+        byte[] fileData = Files.readAllBytes(inAsPath);
 
         // random IV generation
         byte[] iv = new byte[IV_SIZE];
