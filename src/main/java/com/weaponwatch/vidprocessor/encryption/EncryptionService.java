c1bdd6493863d3296b5a95f69bce982d592c5172
chrissu777 <christophersu777@gmail.com>
2024-12-23 17:49:31 -0800

    encryption slop

--- src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
+++ src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
@@ -1,5 +1,8 @@
 package com.weaponwatch.vidprocessor.encryption;
 
+/*
+    TODO: Excpetion Handling
+ */
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
@@ -13,11 +16,18 @@ public class EncryptionService {
     private static final int GCM_TAG_LENGTH = 128;
     private static final int IV_SIZE = 12;
 
-    public void encryptFile(){
+    public void encryptFile(byte[] key) throws Exception{
+
+        // random IV generation
         byte[] iv = new byte[IV_SIZE];
         SecureRandom secureRandom = new SecureRandom();
         secureRandom.nextBytes(iv);
 
+        // cipher initialization
+        GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH, iv);
+        Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION);
+        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, gcmParameterSpec);
 
     }
 }
