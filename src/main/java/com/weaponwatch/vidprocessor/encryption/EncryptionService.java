868f0730663b9ac7a1268ba8419a7844ad0c9573
chrissu777 <christophersu777@gmail.com>
2024-12-23 17:18:45 -0800

    slop

--- src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
+++ src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
@@ -9,6 +9,15 @@ import java.io.FileOutputStream;
 import java.security.SecureRandom;
 
 public class EncryptionService {
-    private final String AES_TRANSFORMATION = "AES/GCM/NoPadding";
+    private static final String AES_TRANSFORMATION = "AES/GCM/NoPadding";
+    private static final int GCM_TAG_LENGTH = 128;
+    private static final int IV_SIZE = 12;
 
+    public void encryptFile(){
+        byte[] iv = new byte[IV_SIZE];
+        SecureRandom secureRandom = new SecureRandom();
+        secureRandom.nextBytes(iv);
+
+
+    }
 }
