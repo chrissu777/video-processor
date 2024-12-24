5face6310837af8b14a07563527fa228854f433a
chrissu777 <christophersu777@gmail.com>
2024-12-23 16:50:33 -0800

    javacrypto

--- src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
+++ src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
@@ -1,5 +1,14 @@
 package com.weaponwatch.vidprocessor.encryption;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.security.SecureRandom;
 
 public class EncryptionService {
+    private final String AES_TRANSFORMATION = "AES/GCM/NoPadding";
+
 }
