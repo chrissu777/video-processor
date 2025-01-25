92174e5d2a61525002fa8a67c3f069b4f881a026
chrissu777 <christophersu777@gmail.com>
2025-01-25 12:34:12 -0500

    changed File usages to Path

--- src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
+++ src/main/java/com/weaponwatch/vidprocessor/encryption/EncryptionService.java
@@ -6,8 +6,6 @@ package com.weaponwatch.vidprocessor.encryption;
 import javax.crypto.Cipher;
 import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,20 +17,26 @@ public class EncryptionService {
     private static final int GCM_TAG_LENGTH = 128;
     private static final int IV_SIZE = 12;
 
-    public void encryptFile(byte[] key, String fileInPath, String fileOutPath) throws Exception{
-        Path inAsPath = Paths.get(fileInPath);
-        byte[] fileData = Files.readAllBytes(inAsPath);
+    public void encryptFile(byte[] key, Path inputPath, Path outputPath) throws Exception{
+        byte[] fileData = Files.readAllBytes(inputPath);
 
-        // random IV generation
-        byte[] iv = new byte[IV_SIZE];
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(iv);
+        Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION); // AES GCM instance
+        byte[] iv = new byte[IV_SIZE]; // random IV generation
+
+        SecureRandom random = new SecureRandom(); // for RNG
+        random.nextBytes(iv);
 
         // cipher initialization
         GCMParameterSpec gcmParameterSpec = new GCMParameterSpec(GCM_TAG_LENGTH, iv);
-        Cipher cipher = Cipher.getInstance(AES_TRANSFORMATION);
         SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, gcmParameterSpec);
 
+        byte[] cipherText = cipher.doFinal(fileData);
+        byte[] outputBytes = new byte[iv.length + cipherText.length];
+
+        System.arraycopy(iv, 0, outputBytes, 0, iv.length);
+        System.arraycopy(cipherText, 0, outputBytes, iv.length, cipherText.length);
+
+        Files.write(outputPath, outputBytes);
     }
 }
